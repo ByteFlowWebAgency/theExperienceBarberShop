07addcc82e8106d8ff7c11f7b2e0f4cce97009a0
Tajohnson245 <tajohnson245@outlook.com>
2025-01-23 19:55:35 -0500

    Fix /franchise

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/Franchise",
+        destination: "/franchise",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
