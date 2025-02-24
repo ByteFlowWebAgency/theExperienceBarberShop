ccbd12ea8b351a99572af8457bc3524b506c76be
Sammieblz <samuelndubuisi32@gmail.com>
2025-02-23 23:29:10 -0500

    import fileURLToPath to sitemap.mjs

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -1,7 +1,11 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const domain = "https://www.theexpshop.com";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pages = [
   "/",
